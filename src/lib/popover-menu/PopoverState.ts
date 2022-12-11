08134a440e05d2507a4feeb1a7b97331e4e02845
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2022-12-11 15:43:19 -0600

    Make popover more dismissable

--- src/lib/popover-menu/PopoverState.ts
+++ src/lib/popover-menu/PopoverState.ts
@@ -16,12 +16,15 @@ import { Projection } from "../projections";
 
 import { generateMenuContent, MenuRow } from "../compute-menu-contents";
 
+// import { createMachine } from "xstate";
+
 export type UpdateDispatch = { from: number; to: number; value: string };
 export type SelectionRoute = [number, number];
 
 export interface PopoverMenuState {
-  showPopover: boolean;
-  popOverInUse: boolean;
+  // showPopover: boolean;
+  // popOverInUse: boolean;
+  menuState: popOverSMState;
   targetNode: SyntaxNode | null;
   targetedTypings: [];
   tooltip: any;
@@ -29,8 +32,9 @@ export interface PopoverMenuState {
   menuContents: MenuRow[];
 }
 export const popoverMenuState: PopoverMenuState = {
-  showPopover: true,
-  popOverInUse: false,
+  // showPopover: true,
+  // popOverInUse: false,
+  menuState: "hidden",
   targetNode: null,
   targetedTypings: [],
   tooltip: null,
@@ -78,13 +82,18 @@ function handleSimpleUpdate(
   let newState = state;
   let didUpdate = false;
   for (const effect of tr.effects) {
-    if (effect.is(setPopoverVisibility)) {
+    // if (effect.is(setPopoverVisibility)) {
+    //   didUpdate = true;
+    //   newState = simpleSet("showPopover", effect.value, newState);
+    // }
+    // if (effect.is(setPopoverUsage)) {
+    //   didUpdate = true;
+    //   newState = simpleSet("popOverInUse", effect.value, newState);
+    // }
+    if (effect.is(popoverEffectDispatch)) {
       didUpdate = true;
-      newState = simpleSet("showPopover", effect.value, newState);
-    }
-    if (effect.is(setPopoverUsage)) {
-      didUpdate = true;
-      newState = simpleSet("popOverInUse", effect.value, newState);
+      const newMenuState = PopoverStateMachine(state.menuState, effect.value);
+      newState = simpleSet("menuState", newMenuState, state);
     }
     if (effect.is(setRouting)) {
       didUpdate = true;
@@ -94,9 +103,86 @@ function handleSimpleUpdate(
   return { newState, didUpdate };
 }
 
+export type popoverSMEvent =
+  | "forceClose"
+  | "forceOpen"
+  | "open"
+  | "close"
+  | "use"
+  | "stopUsing";
+export type popOverSMState = "hardClosed" | "hidden" | "open" | "inUse";
+export const visibleStates = new Set<popOverSMState>(["open", "inUse"]);
+type PartialRecord<K extends keyof any, T> = {
+  [P in K]?: T;
+};
+const stateMap: Record<
+  popOverSMState,
+  PartialRecord<popoverSMEvent, popOverSMState>
+> = {
+  hardClosed: {
+    forceOpen: "open",
+  },
+  hidden: {
+    open: "open",
+  },
+  open: {
+    forceClose: "hardClosed",
+    close: "hidden",
+    use: "inUse",
+  },
+  inUse: {
+    forceClose: "hardClosed",
+    stopUsing: "open",
+    close: "hidden",
+  },
+};
+
+function PopoverStateMachine(
+  state: popOverSMState,
+  event: popoverSMEvent
+): popOverSMState {
+  try {
+    return stateMap[state][event] || state;
+  } catch (e) {
+    console.log("error transitioning", e);
+    return state;
+  }
+}
+
+function computeContents(tr: Transaction, targetNode: SyntaxNode) {
+  const { schemaTypings, diagnostics, projections } =
+    tr.state.field(cmStatePlugin);
+  const fullCode = tr.state.doc.toString();
+  const keyPath = syntaxNodeToKeyPath(targetNode, tr.state);
+  const currentCodeSlice = codeStringState(
+    tr.state,
+    targetNode.from,
+    targetNode.to
+  );
+  return [
+    ...projections
+      .filter((proj) => keyPathMatchesQuery(proj.query, keyPath))
+      .filter((proj) => proj.type === "tooltip")
+      .map(prepProjections(targetNode, keyPath, currentCodeSlice)),
+    ...generateMenuContent(
+      currentCodeSlice,
+      targetNode,
+      schemaTypings,
+      fullCode
+    ),
+    ...diagnostics
+      .filter((x) => x.from === targetNode.from && x.to === targetNode.to)
+      .map((lint) => ({
+        label: "LINT ERROR",
+        elements: [{ type: "display", content: lint.message }],
+      })),
+  ] as MenuRow[];
+}
+
 export const popOverState: StateField<PopoverMenuState> = StateField.define({
   create: () => popoverMenuState,
   update(state, tr) {
+    console.log(state.menuState);
     const simpleUpdate = handleSimpleUpdate(state, tr);
     if (simpleUpdate.didUpdate) {
       return simpleUpdate.newState;
@@ -120,8 +206,7 @@ export const popOverState: StateField<PopoverMenuState> = StateField.define({
     }
 
     // maybe only compute this stuff if the menu is open
-    const { schemaTypings, diagnostics, projections } =
-      tr.state.field(cmStatePlugin);
+    const { schemaTypings } = tr.state.field(cmStatePlugin);
 
     const targetedTypings =
       schemaTypings[`${targetNode.from}-${targetNode.to}`] || [];
@@ -130,64 +215,37 @@ export const popOverState: StateField<PopoverMenuState> = StateField.define({
       targetNode?.from === state?.targetNode?.from &&
       targetNode?.to === state?.targetNode?.to
     );
-    let popOverInUse: boolean = state.popOverInUse;
-    let showPopover: boolean = state.showPopover;
+    // let popOverInUse: boolean = state.popOverInUse;
+    // let showPopover: boolean = state.showPopover;
     let selectedRouting = state.selectedRouting;
+    let menuState = state.menuState;
     if (nodeIsActuallyNew) {
       selectedRouting = [0, 0];
-      popOverInUse = false;
-      showPopover = true;
+      menuState = PopoverStateMachine(state.menuState, "open");
+      // popOverInUse = false;
+      // showPopover = true;
     }
-    // todo probably want to get an xstate type state machine here, this interaction will get pretty intense
 
-    const fullCode = tr.state.doc.toString();
-    const keyPath = syntaxNodeToKeyPath(targetNode, tr.state);
-    const currentCodeSlice = codeStringState(
-      tr.state,
-      targetNode.from,
-      targetNode.to
-    );
-    const menuContents = [
-      ...projections
-        .filter((proj) => keyPathMatchesQuery(proj.query, keyPath))
-        .filter((proj) => proj.type === "tooltip")
-        .map(prepProjections(targetNode, keyPath, currentCodeSlice)),
-      ...generateMenuContent(
-        currentCodeSlice,
-        targetNode,
-        schemaTypings,
-        fullCode
-      ),
-      ...diagnostics
-        .filter((x) => x.from === targetNode.from && x.to === targetNode.to)
-        .map((lint) => ({
-          label: "LINT ERROR",
-          elements: [{ type: "display", content: lint.message }],
-        })),
-    ] as MenuRow[];
-
-    const tooltip = {
-      pos: pos,
-      create: createTooltip(popOverState),
-      above: true,
-    };
     // if were not showing the popover bail
-    if (!showPopover) {
+    if (!visibleStates.has(menuState)) {
       return { ...state, tooltip: null };
     }
+    const tooltip = createTooltip(popOverState);
     return {
       ...state,
-      menuContents,
-      popOverInUse,
+      menuContents: computeContents(tr, targetNode),
+      menuState,
       selectedRouting,
-      showPopover,
       targetNode,
       targetedTypings,
-      tooltip,
+      tooltip: {
+        pos,
+        create: tooltip,
+        above: true,
+      },
     };
   },
   provide: (f) => [showTooltip.from(f, (val) => val.tooltip)],
 });
-export const setPopoverVisibility = StateEffect.define<boolean>();
-export const setPopoverUsage = StateEffect.define<boolean>();
+export const popoverEffectDispatch = StateEffect.define<popoverSMEvent>();
 export const setRouting = StateEffect.define<[number, number]>();
