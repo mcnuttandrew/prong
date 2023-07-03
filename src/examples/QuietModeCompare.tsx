1396a8a54ae5576ae946e4387065380e9e6ce58d
Andrew McNutt <mcnutt.andrew@gmail.com>
2023-07-03 14:44:29 -0500

    add syntax highlighting to the docs (#49)

--- src/examples/QuietModeCompare.tsx
+++ src/examples/QuietModeCompare.tsx
@@ -3,6 +3,7 @@ import QuietModeUs from "./QuietModeUs";
 import QuietModeCodeMirror from "./QuietModeCodeMirror";
 import { produceExample } from "./example-data";
 import "../stylesheets/quiet-styles.css";
+import StyledMarkdown from "./StyledMarkdown";
 
 function QuietModeCompare() {
   const [code, setCode] = useState(produceExample);
@@ -10,36 +11,57 @@ function QuietModeCompare() {
   useEffect(() => {
     Promise.all(
       ["QuietModeCodeMirror.tsx", "QuietModeUs.tsx"].map((el) =>
-        fetch(el).then((x) => x.text())
+        fetch(el)
+          .then((x) => x.text())
+          .then((x) => {
+            console.log(x);
+            // return x;
+            return (
+              "```tsx" +
+              x
+                .replace(
+                  /\n/g,
+                  `
+        `
+                )
+                .split("\n")
+                .map((x) => x.slice(8))
+                .join("\n")
+            );
+          })
       )
     ).then(([codeMirror, us]) => setExamples({ codeMirror, us }));
   }, []);
   return (
-    <div className="flex" id="quiet-root">
+    <div className="flex" id="quiet-root" style={{ width: "100%" }}>
       <div
-        style={{ width: "100%", height: "100%", marginRight: "5px" }}
+        style={{
+          width: "100%",
+          height: "100%",
+          marginRight: "5px",
+          maxWidth: "50%",
+        }}
         className="flex-down"
       >
         <h1>Prong</h1>
         <h3>Lines of code {examples.us.split("\n").length}</h3>
         <QuietModeUs code={code} onChange={setCode} />
         <h3>Raw</h3>
-        <code style={{ height: "100%", overflowY: "scroll" }}>
-          {examples.us.split("\n").map((x, idx) => (
-            <div key={`${idx}-us`}>{x}</div>
-          ))}
-        </code>
+        <div style={{ height: "100%", overflowY: "scroll" }}>
+          <StyledMarkdown content={examples.us} />
+        </div>
       </div>
-      <div style={{ width: "100%", height: "100%" }} className="flex-down">
+      <div
+        style={{ width: "100%", height: "100%", maxWidth: "50%" }}
+        className="flex-down"
+      >
         <h1>Vanilla Code Mirror</h1>
         <h3>Lines of code {examples.codeMirror.split("\n").length}</h3>
         <QuietModeCodeMirror code={code} onChange={setCode} />
         <h3>Raw</h3>
-        <code style={{ height: "100%", overflowY: "scroll" }}>
-          {examples.codeMirror.split("\n").map((x, idx) => (
-            <div key={`${idx}-codemirror`}>{x}</div>
-          ))}
-        </code>
+        <div style={{ height: "100%", overflowY: "scroll" }}>
+          <StyledMarkdown content={examples.codeMirror} />
+        </div>
       </div>
     </div>
   );
---
+++ src/examples/StyledMarkdown.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { xonokai } from "react-syntax-highlighter/dist/esm/styles/prism";
+import ReactMarkdown from "react-markdown";
+
+function StyledMarkdown(props: { content: string }) {
+  return (
+    <ReactMarkdown
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          return !inline ? (
+            <SyntaxHighlighter
+              {...props}
+              children={String(children).replace(/\n$/, "")}
+              style={xonokai}
+              language={"jsx"}
+              PreTag="div"
+            />
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
+      {props.content}
+    </ReactMarkdown>
+  );
+}
+export default StyledMarkdown;
