4e79802a30bb076ca0ae98ce7832988de17dd093
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2023-07-14 10:50:43 -0500

    data placement

--- sites/docs/scripts/do-build.sh
+++ sites/docs/scripts/do-build.sh
@@ -2,15 +2,12 @@
 yarn
 
 # prep assets
-mkdir -p dist/data 
-rm -rf dist/data
-mkdir dist/data 
-cp ./node_modules/vega-datasets/data/* ./dist/data/
+mkdir -p public/data 
+cp ./node_modules/vega-datasets/data/* ./public/data/
 cp ./src/examples/Quiet* ./public/
 cp ../../README.md ./public/
 
 # swap to using the published version of prong edtior
-# npx tsc &&
 yarn add prong-editor &&
 npx vite build &&
 yarn remove prong-editor
