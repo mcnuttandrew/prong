3421685aa18fb5aae2d38e676ecd14bfd05f51de
Andrew McNutt <mcnutt.andrew@gmail.com>
2023-07-19 16:50:23 -0500

    logo adjustments (#63)

--- sites/docs/scripts/do-build.sh
+++ sites/docs/scripts/do-build.sh
@@ -6,6 +6,8 @@ mkdir -p public/data
 cp ./node_modules/vega-datasets/data/* ./public/data/
 cp ./src/examples/Quiet* ./public/
 cp ../../README.md ./public/
+mkdir ./public/public
+cp ../../example.png ./public/
 
 # swap to using the published version of prong edtior
 yarn add prong-editor &&
